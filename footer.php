65da7a1417dbd28598bff0bd1fbe63e0c93819df
Md.Abdur Nur Mondol <103848396+AbdurNur@users.noreply.github.com>
2022-07-26 01:05:25 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!-- Footer Section start -->
+
+<footer class="footer py-4 border-top">
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-lg-12">
+                <p class="text-center m-0 text-muted" >&copy;<?php echo date("Y")." Devloper Nur" ?></p>
+
+            </div>
+        </div>
+    </div>
+
+
+</footer>
+<!-- Footer Section End -->
+
+
+<!-- Bootstrap_5_Cdn -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <!-- Bootstrap_5_Cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+     <!-- Font-awesome-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet" >
+    <!-- Custom Css -->
+    <link href="css/style.css" rel="stylesheet" >
+    <link href="css/responsive.css" rel="stylesheet" >
+   
+    
+</head>
+<body>
+    
